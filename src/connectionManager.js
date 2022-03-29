b595fc5c2db2a504688a80e44ad803b05504693e
Emma Poliakova <2389098p@student.gla.ac.uk>
2022-03-29 17:14:38 +0100

    peer server change

--- src/connectionManager.js
+++ src/connectionManager.js
@@ -1,96 +1,84 @@
-    var el = document.createElement('div');
-    var remotePeerIds = [];
-    var peer;
+var el = document.createElement("div");
+var remotePeerIds = [];
+var peer;
 
-    var stats = new Stats();
-    stats.showPanel(0);
-    
+var stats = new Stats();
+stats.showPanel(0);
 
-    function managerInitialize(url, qrDomElement, displayDomElement){
+function managerInitialize(url, qrDomElement, displayDomElement) {
+  peer = new Peer({
+    secure: true,
+    host: "smartcontrollerserver.herokuapp.com", // change here the herokuapp name
+    port: 443,
+  });
 
-        peer = new Peer(null, {
-            debug: 2
-        }); 
-        
+  peer.on("open", function (id) {
+    console.log("My peer ID is: " + id);
+    createQrCode(url, qrDomElement, displayDomElement);
+  });
 
-        peer.on('open', function(id) {
-            console.log('My peer ID is: ' + id);
-            createQrCode(url, qrDomElement, displayDomElement);
+  peer.on("connection", function (conn) {
+    remotePeerIds.push(conn); // Add remote peer to list
+    var connected = new CustomEvent("open", { detail: "connection closed" });
+    el.dispatchEvent(connected);
+
+    //video calls
+    var getUserMedia =
+      navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia;
+    peer.on("call", function (call) {
+      getUserMedia({ video: false, audio: true }, function () {
+        call.answer(); // Answer the call with an A/V stream.
+        call.on("stream", function (remoteStream) {
+          setRemoteStream(remoteStream);
         });
+      });
+    });
 
-        peer.on('connection',function(conn){
-            
-            remotePeerIds.push(conn); // Add remote peer to list
-            var connected = new CustomEvent('open', {detail : "connection closed"});    
-                el.dispatchEvent(connected);
+    conn.on("open", function () {
+      console.log("Connected with peer: " + conn.peer);
+    });
 
-            //video calls
-            var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-            peer.on('call', function(call) {
-            getUserMedia({video: false, audio: true}, function() {
-                call.answer(); // Answer the call with an A/V stream.
-                call.on('stream', function(remoteStream) {
-                    setRemoteStream(remoteStream)
-                        });
-                    });
-            });
-            
-            conn.on('open', function() {
-                console.log("Connected with peer: "+ conn.peer);
-            });
-                        
-            conn.on('data',function(data){
-                stats.begin();
-                var message = [remotePeerIds.indexOf(conn), data]
-                var connection = new CustomEvent('data', {detail : message});    
-                el.dispatchEvent(connection); 
-                conn.send(Date.now());
-                stats.end();
-            });
-        
-        
-            conn.on('error',function(){
-                console.log("Couldn't connect to " + conn.peer);
-            });
-        
-            conn.on('close',function(){
-                var disconnected = new CustomEvent('close', {detail : "connection closed"});    
-                el.dispatchEvent(disconnected);
-                console.log("Conection "+ conn.peer +" closed")
-            });
-        
-            
-        });
+    conn.on("data", function (data) {
+      stats.begin();
+      var message = [remotePeerIds.indexOf(conn), data];
+      var connection = new CustomEvent("data", { detail: message });
+      el.dispatchEvent(connection);
+      conn.send(Date.now());
+      stats.end();
+    });
 
-    }
+    conn.on("error", function () {
+      console.log("Couldn't connect to " + conn.peer);
+    });
 
+    conn.on("close", function () {
+      var disconnected = new CustomEvent("close", {
+        detail: "connection closed",
+      });
+      el.dispatchEvent(disconnected);
+      console.log("Conection " + conn.peer + " closed");
+    });
+  });
+}
 
+function createQrCode(url, qrDomElement, displayDomElement) {
+  //var qrCode = new QRCode("qrcode", url + "?id=" + peer.id);
+  // return(qrCode);
+  new QRCode(qrDomElement, url + "?id=" + peer.id);
+  console.log(peer.id);
+  displayDomElement.style.display = "block";
+}
 
-    function createQrCode(url, qrDomElement, displayDomElement){
-        //var qrCode = new QRCode("qrcode", url + "?id=" + peer.id);
-       // return(qrCode);
-       new QRCode(qrDomElement, url + "?id=" + peer.id);
-       console.log(peer.id)
-       displayDomElement.style.display = "block";
-    }
+function setRemoteStream(stream) {
+  //var video = videoElement;
+  video.srcObject = stream;
 
-    function setRemoteStream(stream){
-        //var video = videoElement;
-        video.srcObject = stream;
-
-        video.addEventListener("loadedmetadata", function () {
-            scaleX = this.videoWidth; 
-            scaleY = this.videoHeight;
-            video.play();                        
-            main();
-        });
-        
-        
-        } 
-
-
-     
-
-
-            
-
+  video.addEventListener("loadedmetadata", function () {
+    scaleX = this.videoWidth;
+    scaleY = this.videoHeight;
+    video.play();
+    main();
+  });
+}
--- src/peer.js
+++ src/peer.js
@@ -1,60 +1,63 @@
-var el = document.createElement('div');
+var el = document.createElement("div");
 var lastPeerId = null;
 var peer = null; // own peer object
 var conn = null;
 var ping;
-var prevPing; 
+var prevPing;
 var pingElement = document.getElementById("ping");
 
-const queryString = window.location.search;             
+const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-const connectToId = urlParams.get('id')
-
+const connectToId = urlParams.get("id");
 
 function peerInitialize() {
-    // Create own peer object with connection to shared PeerJS server
-    peer = new Peer(null, {
-        debug: 2
-    });
+  // Create own peer object with connection to shared PeerJS server
+  peer = new Peer({
+    secure: true,
+    host: "smartcontrollerserver.herokuapp.com", // change here the herokuapp name
+    port: 443,
+  });
 
-    peer.on('open', function (id) {
-        // Workaround for peer.reconnect deleting previous id
-        if (peer.id === null) {
-            console.log('Received null id from peer open');
-            peer.id = lastPeerId;
-        } else {
-            lastPeerId = peer.id;
-        }
+  peer.on("open", function (id) {
+    // Workaround for peer.reconnect deleting previous id
+    if (peer.id === null) {
+      console.log("Received null id from peer open");
+      peer.id = lastPeerId;
+    } else {
+      lastPeerId = peer.id;
+    }
 
-        console.log('ID: ' + peer.id);
-        join();
-    });
+    console.log("ID: " + peer.id);
+    join();
+  });
 
-    peer.on('connection', function (c) {
-        // Disallow incoming connections
-        c.on('open', function() {
-            c.send("Sender does not accept incoming connections");
-            setTimeout(function() { c.close(); }, 500);
-        });
+  peer.on("connection", function (c) {
+    // Disallow incoming connections
+    c.on("open", function () {
+      c.send("Sender does not accept incoming connections");
+      setTimeout(function () {
+        c.close();
+      }, 500);
     });
-    peer.on('disconnected', function () {
-        console.log('Connection lost. Please reconnect');
+  });
+  peer.on("disconnected", function () {
+    console.log("Connection lost. Please reconnect");
 
-        // Workaround for peer.reconnect deleting previous id
-        peer.id = lastPeerId;
-        peer._lastServerId = lastPeerId;
-        peer.reconnect();
-    });
+    // Workaround for peer.reconnect deleting previous id
+    peer.id = lastPeerId;
+    peer._lastServerId = lastPeerId;
+    peer.reconnect();
+  });
 
-    peer.on('close', function() {
-        conn = null;
-        console.log('Connection destroyed');
-    });
-    peer.on('error', function (err) {
-        console.log(err);
-        alert('' + err);
-    });
-};
+  peer.on("close", function () {
+    conn = null;
+    console.log("Connection destroyed");
+  });
+  peer.on("error", function (err) {
+    console.log(err);
+    alert("" + err);
+  });
+}
 
 /**
  * Create the connection between the two Peers.
@@ -63,45 +66,48 @@ function peerInitialize() {
  * connection and data received on it.
  */
 function join() {
-    // Close old connection
-    if (conn) {
-        conn.close();
-    }
+  // Close old connection
+  if (conn) {
+    conn.close();
+  }
 
-    // Create connection to destination peer specified in the input field
-    conn = peer.connect(connectToId, {
-        reliable: true
-    });
+  // Create connection to destination peer specified in the input field
+  conn = peer.connect(connectToId, {
+    reliable: true,
+  });
 
-    conn.on('open', function () {
-        console.log("Connected to: " + conn.peer);
+  conn.on("open", function () {
+    console.log("Connected to: " + conn.peer);
+  });
 
-    });
+  // Handle incoming data (messages only since this is the signal sender)
+  conn.on("data", function (data) {
+    ping = Date.now() - prevPing;
+    console.log(ping + " ms");
+    pingElement.innerHTML = ping + " ms";
+  });
 
-    // Handle incoming data (messages only since this is the signal sender)
-    conn.on('data', function (data) {
-        ping = Date.now() - prevPing;
-        console.log(ping + " ms");
-        pingElement.innerHTML = ping + " ms";
+  conn.on("close", function () {});
+}
 
-    });
-
-    conn.on('close', function () {
-        
-    });
-};
-
-function makeCall(videoElement){
-    var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-    getUserMedia({video: true, audio: false}, function(stream) {
-    var call = peer.call(conn.peer, stream);
-    var video = videoElement;
-    video.srcObject = stream;
-    video.play();
-    call.on('stream', function(remoteStream) {
+function makeCall(videoElement) {
+  var getUserMedia =
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia;
+  getUserMedia(
+    { video: true, audio: false },
+    function (stream) {
+      var call = peer.call(conn.peer, stream);
+      var video = videoElement;
+      video.srcObject = stream;
+      video.play();
+      call.on("stream", function (remoteStream) {
         // Show stream in some video/canvas element.
-    });
-    }, function(err) {
-    console.log('Failed to get local stream' ,err);
-    });
+      });
+    },
+    function (err) {
+      console.log("Failed to get local stream", err);
+    }
+  );
 }
